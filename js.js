e580972f08e74b0f10705300f80ad87a39b0e13f
Slava253 <131888809+Slava253@users.noreply.github.com>
2026-07-20 22:43:29 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBfoIEyhvlUfIgAvXJwOthT6uzfB4gV0Uo",
+    authDomain: "electronic-journal-5dff1.firebaseapp.com",
+    databaseURL: "https://electronic-journal-5dff1-default-rtdb.firebaseio.com",
+    projectId: "electronic-journal-5dff1",
+    storageBucket: "electronic-journal-5dff1.firebasestorage.app",
+    messagingSenderId: "293962922085",
+    appId: "1:293962922085:web:391c3e453bb2725e392e8c"
+};
