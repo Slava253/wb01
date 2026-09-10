2a2c7e290db9d7b764b3768da3ac1a1108926179
Slava253 <131888809+Slava253@users.noreply.github.com>
2026-09-10 17:45:49 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-import {auth,db,doc,getDoc} from "./firebase.js";
-import {onAuthStateChanged} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-onAuthStateChanged(auth,async u=>{
- if(!u){location.href="login.html";return}
- const s=await getDoc(doc(db,"users",u.uid));
- const p=s.exists()?s.data():null;
- if(p?.role==="admin") location.href="admin.html";
- else if(p?.role==="employee") location.href="employee.html";
- else location.href="client.html";
+import { auth, db, doc, getDoc } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+onAuthStateChanged(auth, async u => {
+  if (!u) { location.href = "login.html"; return; }
+  const s = await getDoc(doc(db, "users", u.uid));
+  const p = s.exists() ? s.data() : null;
+  if (p?.role === "admin") location.href = "admin.html";
+  else if (p?.role === "employee") location.href = "employee.html";
+  else location.href = "client.html";
 });
