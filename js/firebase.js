002a8f42fec5b1ad04e4ad8afdce2e032503008b
Slava253 <131888809+Slava253@users.noreply.github.com>
2026-09-10 19:24:02 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,10 +3,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
 import {
-    getAuth
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-import {
     getFirestore
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
@@ -31,7 +27,5 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
 
 export const db = getFirestore(app);
