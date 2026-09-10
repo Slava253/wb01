5b040b2f3e14dbab65e10a1a37dbc085851ad26b
Slava253 <131888809+Slava253@users.noreply.github.com>
2026-09-10 20:40:13 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// ============================================================
+// Firebase config — проект marketpoint-9be60
+// ============================================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYFjKU5odBSUHLhieXdc0dr06EhLM_3r0",
+  authDomain: "marketpoint-9be60.firebaseapp.com",
+  projectId: "marketpoint-9be60",
+  storageBucket: "marketpoint-9be60.firebasestorage.app",
+  messagingSenderId: "159645831524",
+  appId: "1:159645831524:web:252097b1aa1f3495836a71",
+  measurementId: "G-14LM5KL3J0"
+};
